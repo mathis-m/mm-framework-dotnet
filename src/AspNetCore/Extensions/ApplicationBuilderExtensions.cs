b8425c7b8dc6dea0c43df3facae710f65decdd07
mathis-m <mathis.michel@outlook.de>
2020-09-16 20:00:41 +0200

    add IApplicationBuilder extension to configure MMFramework hosting

---
+++ src/AspNetCore/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using MMFramework.AspNetCore.Builder;
+
+namespace MMFramework.AspNetCore.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+
+        /// <summary>
+        /// Use MMFramework to configure hosting./>
+        /// </summary>
+        /// <param name="app">The <see cref="IApplicationBuilder"/> instance to be configured.</param>
+        /// <returns>Returns <see cref="IMMApplicationBuilder"/> to configure the <see cref="IApplicationBuilder"/></returns>
+        public static IMMApplicationBuilder UseMMFramework(this IApplicationBuilder app)
+        {
+            return new MMApplicationBuilder(app);
+        }
+    }
+}
