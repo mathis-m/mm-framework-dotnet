05dcb25474490390b0f42e8e8aabcee7c74f797a
mathis-m <mathis.michel@outlook.de>
2020-09-16 19:48:55 +0200

    create di extension entry point.
    introducing the MMServiceBuilder, used to configure MMFramework related services

---
+++ src/DependencyInjection/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MMFramework.DependencyInjection.Builder;
+
+namespace MMFramework.DependencyInjection.Extensions
+{
+    public static class DependencyInjectionExtensions
+    {
+        /// <summary>
+        ///     Adds MMFramework services to the specified IServiceCollection.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
+        /// <returns>An <see cref="IMMServiceBuilder"/> that can be used to further configure the MMFramework services.</returns>
+        public static IMMServiceBuilder AddMMFramework(this IServiceCollection services)
+        {
+            return new MMServiceBuilder(services);
+        }
+    }
+}
