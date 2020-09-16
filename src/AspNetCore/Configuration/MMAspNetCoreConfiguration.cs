fee1a547803f6b5db51eeacb5e6172aaf6c53085
mathis-m <mathis.michel@outlook.de>
2020-09-16 20:01:11 +0200

    added basic mm hosting configuration

---
+++ src/AspNetCore/Configuration/MMAspNetCoreConfiguration.cs
@@ -0,0 +1,22 @@
+﻿
+namespace MMFramework.AspNetCore.Configuration
+{
+    public class MMAspNetCoreConfiguration : IMMAspNetCoreConfiguration
+    {
+        public string BasePath { get; set; }
+
+        public MMAspNetCoreConfiguration(string serviceName, string serviceVersion, bool isDevelopment)
+        {
+            serviceName = serviceName
+                .Trim('/')
+                .ToLower();
+            serviceVersion = serviceVersion
+                .Trim('/')
+                .ToLower()
+                .TrimStart('v');
+            BasePath = isDevelopment
+                ? ""
+                : $"/{serviceName}/v{serviceVersion}";
+        }
+    }
+}
