9edaa4de15256f83750ac1f90c0e89534abcc924
mathis-m <mathis.michel@outlook.de>
2020-09-16 20:02:49 +0200

    base swagger integrations

---
+++ src/Swashbuckle/Configuration/MMSwashbuckleConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using MMFramework.Swashbuckle.Configuration.SortConfiguration;
+
+namespace MMFramework.Swashbuckle.Configuration
+{
+    public class MMSwashbuckleConfiguration : IMMSwashbuckleConfiguration
+    {
+        public string ServiceName { get; set; }
+        public string ServiceVersion { get; set; }
+        public IMMSortSwaggerConfiguration SortConfiguration { get; set; }
+
+        public MMSwashbuckleConfiguration(string serviceName, string serviceVersion)
+        {
+            ServiceName = serviceName;
+            ServiceVersion = serviceVersion;
+            SortConfiguration = new MMSortSwaggerConfiguration();
+        }
+    }
+}
