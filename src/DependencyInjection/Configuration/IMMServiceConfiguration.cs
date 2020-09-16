3f974cc1d2604f033896ad2e8916a0d99359245c
mathis-m <mathis.michel@outlook.de>
2020-09-16 19:51:39 +0200

    Base Service Configuration
    introducing swashbuckle and aspnetcore integration

---
+++ src/DependencyInjection/Configuration/IMMServiceConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using MMFramework.AspNetCore.Configuration;
+using MMFramework.Swashbuckle.Configuration;
+
+namespace MMFramework.DependencyInjection.Configuration
+{
+    public interface IMMServiceConfiguration
+    {
+
+        /// <summary>
+        ///     Optional configuration for swashbuckle setup.
+        /// </summary>
+        IMMSwashbuckleConfiguration? SwashbuckleConfiguration { get; set; }
+
+        /// <summary>
+        ///     Optional configuration for aspnetcore setup.
+        /// </summary>
+        IMMAspNetCoreConfiguration? AspNetCoreConfiguration { get; set; }
+    }
+}
