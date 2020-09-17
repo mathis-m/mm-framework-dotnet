b86d6a977fa9296cdc10f259cfd036d70de1015d
mathis-m <mathis.michel@outlook.de>
2020-09-17 16:56:47 +0200

    add di abstractions

---
+++ abstractions/DependencyInjection.Abstractions/Builder/IMMServiceBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MMFramework.DependencyInjection.Builder
+{
+    public interface IMMServiceBuilder
+    {
+        IServiceCollection Services { get; }
+        public IMMServiceBuilder AddMMServiceSetupAction(Action setupAction);
+        IServiceCollection Build();
+    }
+}
