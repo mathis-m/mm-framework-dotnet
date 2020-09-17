f26116ad2d47519abde3cc5701e47be53d8aac52
mathis-m <mathis.michel@outlook.de>
2020-09-17 14:11:57 +0200

    move swagger parts to swagger project

---
+++ src/Swashbuckle/Extensions/SwaggerMMApplicationBuilderExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using MMFramework.AspNetCore.Builder;
+using MMFramework.Swashbuckle.Configuration;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace MMFramework.Swashbuckle.Extensions
+{
+    public static class SwaggerMMApplicationBuilderExtension
+    {
+        public static IMMSwashbuckleConfiguration SwaggerConfig(this IMMApplicationBuilder builder) => builder.ServiceProvider.GetRequiredService<IMMSwashbuckleConfiguration>();
+
+        public static IMMApplicationBuilder UseSwagger(this IMMApplicationBuilder builder)
+        {
+            return builder.UseSwagger(null);
+        }
+
+        public static IMMApplicationBuilder UseSwagger(this IMMApplicationBuilder builder, Action<SwaggerUIOptions>? setupAction)
+        {
+            var swaggerConfig = builder.SwaggerConfig();
+
+            builder.App
+                .UseSwagger()
+                .UseSwaggerUI(c =>
+                {
+                    setupAction?.Invoke(c);
+                    c.SwaggerEndpoint(swaggerConfig.SwaggerEndpoint, swaggerConfig.SwaggerEndpointName);
+                    c.RoutePrefix = builder.AspConfig.BasePath;
+                });
+            return builder;
+        }
+    }
+}
---
+++ src/Swashbuckle/Extensions/SwaggerMMServiceBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.OpenApi.Models;
+using MMFramework.DependencyInjection.Builder;
+using MMFramework.Swashbuckle.Configuration;
+
+namespace MMFramework.Swashbuckle.Extensions
+{
+    public static class SwaggerMMServiceBuilderExtensions
+    {
+        public static IMMServiceBuilder AddSwaggerIntegration(this IMMServiceBuilder builder, string serviceName, string serviceVersion)
+        {
+            var config = new MMSwashbuckleConfiguration(serviceName, serviceVersion);
+            builder.AddMMServiceSetupAction(() =>
+            {
+                builder.Services.TryAddSingleton<IMMSwashbuckleConfiguration>(config);
+                builder.Services.AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc(config.MajorServiceVersionForUrl(), config.OpenApiInfo);
+                });
+            });
+            return builder;
+        }
+
+        public static IMMServiceBuilder AddSwaggerIntegration(this IMMServiceBuilder builder, string serviceName, string serviceVersion, OpenApiInfo openApiInfo)
+        {
+            var config = new MMSwashbuckleConfiguration(serviceName, serviceVersion, openApiInfo);
+            builder.AddMMServiceSetupAction(() =>
+            {
+                builder.Services.TryAddSingleton<IMMSwashbuckleConfiguration>(config);
+                builder.Services.AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc(config.MajorServiceVersionForUrl(), config.OpenApiInfo);
+                });
+            });
+            return builder;
+        }
+    }
+}
