734140a65f00e7ac3e91ca9b36891d64c89f40e3
mathis-m <mathis.michel@outlook.de>
2020-09-17 16:09:56 +0200

    change default sort config

--- src/Swashbuckle/Configuration/SortConfiguration/MMSortSwaggerConfiguration.cs
+++ src/Swashbuckle/Configuration/SortConfiguration/MMSortSwaggerConfiguration.cs
@@ -2,8 +2,7 @@
 {
     public class MMSortSwaggerConfiguration : IMMSortSwaggerConfiguration
     {
-        public bool SortByController { get; set; } = true;
         public bool ThenByComplexity { get; set; } = true;
-        public bool DeprecatedLast { get; set; } = true;
+        public bool DeprecatedLast { get; set; } = false;
     }
 }
