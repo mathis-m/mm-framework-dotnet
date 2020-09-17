3a87c81a1b179be87b61f36fbf88a1bced696e78
mathis-m <mathis.michel@outlook.de>
2020-09-17 17:29:58 +0200

    refactor filter

--- src/Swashbuckle/Filter/SwaggerSortByComplexityFilter.cs
+++ src/Swashbuckle/Filter/SwaggerSortByComplexityFilter.cs
@@ -18,51 +18,23 @@ namespace MMFramework.Swashbuckle.Filter
         public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
         {
 
-            IOrderedEnumerable<KeyValuePair<string, OpenApiPathItem>>? paths = swaggerDoc.Paths
+            var paths = swaggerDoc.Paths
                 .OrderBy(e => e.Key); // alphabetic
 
             // Sort on operation level for deprecation
-            if (_configuration.SortConfiguration.DeprecatedLast)
+            var byComplexity = _configuration.SortConfiguration.ThenByComplexity;
+            var deprecatedLast = _configuration.SortConfiguration.DeprecatedLast;
+            if (deprecatedLast)
             {
-                paths = swaggerDoc.Paths
-                    .Select(x =>
-                    {
-                        var ops = new Dictionary<OperationType, OpenApiOperation>();
-                        var tmpOps = x.Value.Operations
-                            .OrderBy(op => op.Value.Deprecated);
-                        // and complexity
-                        if (_configuration.SortConfiguration.ThenByComplexity)
-                        {
-                            tmpOps = tmpOps.ThenBy(op => op.Value.Parameters.Count);
-                        }
-                        
-                        tmpOps
-                            .ToList()
-                            .ForEach(op => ops.Add(op.Key, op.Value));
-                        x.Value.Operations = ops;
-                        return x;
-                    })
-                    .OrderBy(x => x.Value.Operations.Values.Any(op => op.Deprecated)) // deprecated last
-                    .ThenBy(e => e.Key); // alphabetic
+                paths = SortOperationsDeprecatedLastThenByComplexity(swaggerDoc); // alphabetic
             }
             // for only complexity
-            else if (_configuration.SortConfiguration.ThenByComplexity)
+            else if (byComplexity)
             {
-                paths = swaggerDoc.Paths
-                    .Select(x =>
-                    {
-                        var ops = new Dictionary<OperationType, OpenApiOperation>();
-                        x.Value.Operations
-                            .OrderBy(op => op.Value.Parameters.Count)
-                            .ToList()
-                            .ForEach(op => ops.Add(op.Key, op.Value));
-                        x.Value.Operations = ops;
-                        return x;
-                    })
-                    .OrderBy(e => e.Key);
+                paths = SortOperationsOnlyByComplexity(swaggerDoc);
             }
             // sort paths by complexity
-            if (_configuration.SortConfiguration.ThenByComplexity)
+            if (byComplexity)
             {
                 paths = paths
                     .ThenBy(x => x.Key.Length + x.Value.Parameters.Count); // complexity
@@ -71,6 +43,48 @@ namespace MMFramework.Swashbuckle.Filter
             var newPaths = new OpenApiPaths();
             paths.ToList().ForEach(path => newPaths.Add(path.Key, path.Value));
             swaggerDoc.Paths = newPaths;
+        }
+
+        private static IOrderedEnumerable<KeyValuePair<string, OpenApiPathItem>> SortOperationsOnlyByComplexity(OpenApiDocument swaggerDoc)
+        {
+            return swaggerDoc.Paths
+                .Select(SortDeprecatedOperationsLast)
+                .OrderBy(e => e.Key);
+        }
+
+        private static KeyValuePair<string, OpenApiPathItem> SortDeprecatedOperationsLast(KeyValuePair<string, OpenApiPathItem> x)
+        {
+            var ops = new Dictionary<OperationType, OpenApiOperation>();
+            x.Value.Operations
+                .OrderBy(op => op.Value.Parameters.Count)
+                .ToList()
+                .ForEach(op => ops.Add(op.Key, op.Value));
+            x.Value.Operations = ops;
+            return x;
+        }
+
+        private IOrderedEnumerable<KeyValuePair<string, OpenApiPathItem>> SortOperationsDeprecatedLastThenByComplexity(OpenApiDocument swaggerDoc)
+        {
+            return swaggerDoc.Paths
+                .Select(SortOperationsDeprecatedLastAndOptionalByComplexity)
+                .OrderBy(x => x.Value.Operations.Values.Any(op => op.Deprecated)) // deprecated last
+                .ThenBy(e => e.Key);
+        }
+
+        private KeyValuePair<string, OpenApiPathItem> SortOperationsDeprecatedLastAndOptionalByComplexity(KeyValuePair<string, OpenApiPathItem> x)
+        {
+            var ops = new Dictionary<OperationType, OpenApiOperation>();
+            var tmpOps = x.Value.Operations.OrderBy(op => op.Value.Deprecated);
+            // and complexity
+            if (_configuration.SortConfiguration.ThenByComplexity)
+            {
+                tmpOps = tmpOps.ThenBy(op => op.Value.Parameters.Count);
+            }
+
+            tmpOps.ToList()
+                .ForEach(op => ops.Add(op.Key, op.Value));
+            x.Value.Operations = ops;
+            return x;
         }
     }
 }
